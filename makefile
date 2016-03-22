78d673e3b82124ccb89b7384ce093fbd5a336ca5
eldron <eldron@163.com>
2016-03-22 02:27:29 -0700

    c

--- makefile
+++ makefile
@@ -1,6 +1,7 @@
-learn:
+all:
 	gcc learn.c -o learn -lpcap
+	gcc process.c -o process -lpcap
 
 clean:
 	rm -f learn
-	
+	rm -f process
--- process.c
+++ process.c
@@ -17,6 +17,10 @@ int adjacent_matrix[256][256];
 double vector[65536];
 double entropy;
 
+char buffer[65536];
+int payload_length;
+char filename[100];
+
 void process_ip_packet(struct ip * ip_hdr, FILE * file){
 	struct tcphdr * tcp_hdr = NULL;
 	struct udphdr * udp_hdr = NULL;
@@ -107,9 +111,10 @@ void process_ip_packet(struct ip * ip_hdr, FILE * file){
 	}
 }
 
+// 
 int main(int argc, char ** args){
 	if(argc != 5){
-		printf("usage: ./process vector_length matrix_file packets_path result_file\n");
+		printf("usage: ./process vector_length matrix_file packets_path payload_length\n");
 		return 0;
 	}
 
@@ -124,9 +129,10 @@ int main(int argc, char ** args){
 			}
 		}
 	} else {
-		printf("cano not open file %s\n", args[2]);
+		printf("can not open file %s\n", args[2]);
 		return 0;
 	}
+	payload_length = atoi(args[4]);
 
 	DIR * dir = opendir(args[3]);
 	if(dir){
@@ -137,34 +143,85 @@ int main(int argc, char ** args){
 		struct pcap_pkthdr header;
 		struct ether_header * eth_hdr = NULL;
 		struct ip * ip_hdr = NULL;
+		struct tcphdr * tcp_hdr = NULL;
+		struct udphdr * udp_hdr = NULL;
+		char * data = NULL;
+		int data_len = 0;
 
 		while((ent = readdir(dir)) != NULL){
 			pcap = pcap_open_offline(ent->d_name, errbuf);
 			if(pcap){
-				while((packet = pcap_next(pcap, &header)) != NULL){
-						// count adjacent byte frequencies
-						if(header.caplen < sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr)){
-							printf("packet too short\n");
-						} else {
-							eth_hdr = (struct ether_header *) packet;
-							unsigned short ether_type = SWAP_SHORT(eth_hdr->ether_type);
-							if(ether_type == 0x0800){
-								// ip packet
-								ip_hdr = (struct ip *) (packet + sizeof(struct ether_header));
-								process_ip_packet(ip_hdr);
-							} else if(ether_type == 0x8100){
-								// vlan packet
-								unsigned short * vlan_proto = (unsigned short *) (packet + sizeof(struct ether_header) + 2);
-								if(*vlan_proto == 0x0800){
-									vlan_packets_count++;
-									ip_hdr = (struct ip *) (packet + sizeof(struct ether_header) + 4);
-									process_ip_packet(ip_hdr);
-								}
-							} else {
+				int bytes_count = 0;
 
+				while((packet = pcap_next(pcap, &header)) != NULL){
+					if(header.caplen < sizxeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr)){
+						printf("packet too short");
+					} else {
+						eth_hdr = (struct ether_header *) packet;
+						unsigned short ether_type = SWAP_SHORT(eth_hdr->ether_type);
+						if(ether_type == 0x0800){
+							// ip packet
+							ip_hdr = (struct ip *) (packet + sizeof(struct ether_header));
+						} else if(ether_type == 0x8100){
+							// vlan packet
+							unsigned short * vlan_proto = (unsigned short *) (packet + sizeof(struct ether_header) + 2);
+							if(*vlan_proto == 0x0800){
+								ip_hdr = (struct ip *) (packet + sizxeof(struct ether_header) + 4);
+							} else {
+								ip_hdr = NULL;
+							}
+						} else {
+							ip_hdr = NULL;
+						}
+
+						if(ip_hdr){
+							if(ip_hdr->ip_v == 4){
+								data = NULL;
+								data_len = 0;
+								if(ip_hdr->ip_p == 6){
+									char * tmp = (char *) ip_hdr;
+			 						tcp_hdr = (struct tcphdr *) (tmp + ip_hdr->ip_hl * 4);
+									data_len = ip_hdr->ip_len - ip_hdr->ip_hl * 4 - tcp_hdr->th_off * 4;
+									if(data_len > 0){
+			 							data = tmp + ip_hdr->ip_hl * 4 + tcp_hdr->th_off * 4;
+			 						}
+								} else if(ip_hdr->ip_p == 17){
+									char * tmp = (char *) ip_hdr;
+									data_len = ip_hdr->ip_len - ip_hdr->ip_hl * 4 - sizeof(struct udphdr);
+									if(data_len > 0){
+										data = tmp + ip_hdr->ip_hl * 4 + sizeof(struct udphdr);
+									}
+								}
+
+								if(data){
+									int tocopy = 0;
+									if(payload_length - bytes_count > data_len){
+										tocopy = data_len;
+									} else {
+										tocopy = payload_length - bytes_count;
+									}
+									int i;
+									for(i = 0;i < tocopy;i++){
+										buffer[bytes_count + i] = data[i];
+									}
+									bytes_count += tocopy;
+									if(bytes_count == payload_length){
+										break;
+									}
+								}
 							}
 						}
 					}
+				}
+
+				if(bytes_count == payload_length){
+					strcpy(filename, ent->d_name);
+					strcat(filename, ".result");
+					FILE * file = fopen(filename, "w");
+					// calculate entropy and byte adjacent information, write the result to file
+					int bit_array[10000];
+					
+				}
 			} else {
 				printf("can not open file %s\n", ent->d_name);
 			}

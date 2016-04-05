77be622ba8f4c71e4947a4bdc574609f3d72a382
eldron <eldron@163.com>
2016-04-05 09:51:46 +0800

    c

---
+++ entropy_result7/hello.sh
@@ -0,0 +1,15 @@
+#########################################################################
+# File Name: hello.sh
+# Author: liusong
+# mail: liusong_ee@163.com
+# Created Time: Thu 31 Mar 2016 09:14:49 PM CST
+#########################################################################
+#!/bin/bash
+
+python ntrunk.py > 7.txt &
+python ntrunk1.py > 1.txt &
+python ntrunk2.py > 2.txt &
+python ntrunk3.py > 3.txt &
+python ntrunk4.py > 4.txt &
+python ntrunk5.py > 5.txt &
+python ntrunk6.py > 6.txt &
---
+++ entropy_result7/ntrunk5.py
@@ -0,0 +1,34 @@
+from __future__ import division
+import random
+import math
+import sys
+
+def cal_entropy(N):
+	list = []
+	for i in range(N):
+		list.append(random.randint(0, 255))
+
+	counters = {}
+	for i in list:
+		if(i in counters.keys()):
+			counters[i] += 1
+		else:
+			counters[i] = 1
+
+	entropy = float(0)
+	for i in counters.values():
+		tmp = (i / N) * math.log(i / N, 2)
+		entropy += tmp
+	return float(float(0) - entropy)
+
+def cal_ntrunk(N):
+	values = []
+	for i in range(100000):
+		values.append(cal_entropy(N))
+
+	return float(sum(values) / 100000)
+
+for i in range(1001, 1200):
+	value = cal_ntrunk(i)
+	print value
+	sys.stderr.write(str(value) + "\n")
